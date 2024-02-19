c4e8a8d6e6113186336a8dbe6d7cd243d2b6f655
Lucas Araujo <37451939+lucasaraujonrt@users.noreply.github.com>
2024-02-19 20:06:06 -0300

    fix: typo

--- app/api/clients/[id]/route.ts
+++ app/api/clients/[id]/route.ts
@@ -1,10 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("req", req);
+export default async function handler(req: NextApiRequest) {
   if (req.method === "PUT") {
     const { client } = req.body;
 
