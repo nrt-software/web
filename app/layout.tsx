fb70004c57e60a290857be023b0db40c3cfc25e4
Lucas Araujo <37451939+lucasaraujonrt@users.noreply.github.com>
2024-02-19 20:08:17 -0300

    feat: add insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import { Inter as FontSans } from "next/font/google";
@@ -38,6 +39,7 @@ export default function RootLayout({
         >
           <Toaster />
           <Provider>{children}</Provider>
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
